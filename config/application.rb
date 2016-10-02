184d8d32b9012d45c2a932be7368cfec3bcf64c7
Cody Tanner <ctanner@instructure.com>
2016-10-02 07:07:00 -0600

    setting up landing page

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,12 @@ module Lynsey
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
+      config.assets.paths << path
+    end
+
+    Dir.glob("#{Rails.root}/app/assets/fonts/**/").each do |path|
+      config.assets.paths << path
+    end
   end
 end
